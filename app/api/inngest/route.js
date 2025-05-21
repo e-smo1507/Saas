6fdd2c415740547a7368a590f3d39a93a63e6a4f
e-smo1507 <esmoliguni@gmail.com>
2025-05-21 19:28:15 +0530

    yet signed it data not showing

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,9 @@
 import { serve } from "inngest/next";
 
-import {inngest} from "@/lib/inngest/client";
+import { inngest } from "@/lib/inngest/client";
 import { generateIndustryInsights } from "@/lib/inngest/function";
-// Create an API that serves zero functions
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  
-    /* your functions will be passed here later! */
- functions: [generateIndustryInsights],
-
+  functions: [generateIndustryInsights],
 });
--- app/layout.js
+++ app/layout.js
@@ -35,7 +35,7 @@ export default function RootLayout({ children }) {
             <main className="min-h-screen">  {children}</main>
             <Toaster richColors/>
       
-          <footer calssName="bg-muted/50 py-12">
+          <footer className="bg-muted/50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-200">
              
             </div>
